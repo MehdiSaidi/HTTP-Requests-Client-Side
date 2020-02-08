398a2e631fe2bde7fed3af9880909843485134ef
Mehdi Saidi <mehdisaidi879@gmail.com>
2020-02-08 17:27:05 -0500

    fixed issues in the RequestHandler class

--- src/Handlers/RequestHandler.java
+++ src/Handlers/RequestHandler.java
@@ -1,6 +1,7 @@
 package Handlers;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -21,20 +22,26 @@ public class RequestHandler {
     private static String headers = "";
 
     public static void handleRequest(String[] args) throws IOException, URISyntaxException {
+        boolean urlGood = false;
 
         // 1) Get method (GET or POST)
         method = args[0];
 
-        if (method.contains("help")) {
-            if (args[1].contains("get"))
+        if (method.equals("help")) {
+            if (args[1].equals("get"))
                 Helper.helpGet();
-            else if (args[1].contains("post"))
+            else if (args[1].equals("post"))
                 Helper.helpPost();
         } else
             method = method.toUpperCase();
 
         // 2) Get arguments
-        int urlIndex = handleArguments(args);
+        int urlIndex;
+
+        if(handleArguments(args) == 1){
+            urlIndex = 1;
+        } else
+            urlIndex = handleArguments(args) - 1;
 
         // 3) Get URL and path (Use URL and URI classes)
         // TODO: I think there might be something wrong with how we are getting the URL
@@ -43,18 +50,23 @@ public class RequestHandler {
         // TODO: Hacky way of doing it, let's keep an eye on this
         web = web.replaceAll("'", "");
 
-        URL urlObject = new URL(web);
 
-        web = urlObject.getHost();
-        // TODO: If in the command line the user already enter a URL with www.
-        // this will add an extra www. so it will become -> www.www.
-        web = "www." + web;
+        //--- the purpose of this loop is to retry once the exception ahs been catched.
+        while(!urlGood) {
+            try {
+                URL urlObject = new URL(web);
 
-        urlPath = urlObject.getFile();
+                web = urlObject.getHost();
+                urlGood = true;
 
-        // 4) Put together the request message with the correct format
-        requestMessage = method + " " + urlPath + " " + httpVersion + "\r\n" + headers + entityBody + "\r\n\r\n";
+                urlPath = urlObject.getFile();
 
+                // 4) Put together the request message with the correct format
+                requestMessage = method + " " + urlPath + " " + httpVersion + "\r\n" + headers + entityBody + "\r\n";
+            } catch (MalformedURLException e) {
+                web = "http://" + web;
+            }
+        }
     }
 
     private static int handleArguments(String[] args) throws IOException {
@@ -62,9 +74,11 @@ public class RequestHandler {
         ArrayList<String> headerArr = new ArrayList<String>();
         int i = 1;
 
-        // TODO: Fix -> The URL is not always going to contain http
-        if (!args[1].contains("http")) {
-            for (i = 1; i < args.length && !args[i].contains("http"); i++) {
+        // TODO: Fix -> The URL is not always going to contain http ------------------------------------------> fixed
+        if (!args[1].contains("http") && args[1].equals("-d")|| args[1].equals("-h")|| args[1].equals("-f")||
+                args[1].equals("-v")) {
+
+            for (i = 1; i < args.length; i++) {
 
                 // Argument -v
                 if (args[i].equals("-v")) {
@@ -102,9 +116,9 @@ public class RequestHandler {
 
                     entityBody = args[i + 1];
 
-                    // TODO: Fix this. It's not always going to be in JSON format {Assignment : 1}
+                    // TODO: Fix this. It's not always going to be in JSON format {Assignment : 1} ---------------> fixed
                     // it's just a string
-                    if (!entityBody.contains("}")) {
+                    if (!entityBody.contains(" ' ")) {
                         i = i + 2;
                         while (!args[i].contains("-d") && !args[i].contains("-v") && !args[i].contains("-h")
                                 && !args[i].contains("http")) {
@@ -112,6 +126,7 @@ public class RequestHandler {
                             entityBody = entityBody + args[i];
                             i++;
                         }
+                        entityBody = entityBody.replaceAll("'", "");
                     }
                     continue;
                 }
